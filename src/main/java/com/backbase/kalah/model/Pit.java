d6345c32dba009574448bf3239fa64601a2e8cbe
ru28cf <ugur.taskan@ing.com>
2020-01-03 16:09:16 +0100

    Correction of jackson dependency

--- src/main/java/com/backbase/kalah/model/Pit.java
+++ src/main/java/com/backbase/kalah/model/Pit.java
@@ -2,11 +2,7 @@ package com.backbase.kalah.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class Pit {
 
     private final int id;
@@ -20,6 +16,10 @@ public class Pit {
         }
     }
 
+    public int getId() {
+        return this.id;
+    }
+
     public Player getOwner() {
         if (this.getId() <= Player.PLAYER_NORTH.getHouseIndex()) {
             return Player.PLAYER_NORTH;
@@ -28,15 +28,23 @@ public class Pit {
         }
     }
 
+    public int getStoneCount() {
+        return this.stoneCount;
+    }
+
+    public void setStoneCount(final int stoneCount) {
+        this.stoneCount = stoneCount;
+    }
+
     public boolean isDistributable(final Player turn) {
         return (!turn.equals(Player.PLAYER_NORTH)
                 || (this.getId() != Player.PLAYER_SOUTH.getHouseIndex()))
-               && (!turn.equals(Player.PLAYER_SOUTH)
-                   || (this.getId() != Player.PLAYER_NORTH.getHouseIndex()));
+                && (!turn.equals(Player.PLAYER_SOUTH)
+                || (this.getId() != Player.PLAYER_NORTH.getHouseIndex()));
     }
 
     public boolean isHouse() {
         return (this.getId() == Player.PLAYER_NORTH.getHouseIndex())
-               || (this.getId() == Player.PLAYER_SOUTH.getHouseIndex());
+                || (this.getId() == Player.PLAYER_SOUTH.getHouseIndex());
     }
 }
